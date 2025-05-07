66867fa2a7c00eca2e3bc63e3ab4279dbeaedaa5
fj200 <folajinmiadewole@gmail.com>
2025-05-07 16:47:00 -0400

    Set up the directory

---
+++ healthprovider-service/src/main/java/com/project/healthprovider/HealthproviderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.healthprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthproviderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthproviderServiceApplication.class, args);
+	}
+
+}
